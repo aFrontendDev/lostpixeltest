5e9b81e07bc134f78a9f7b6c892ae8b4b3295e5c
Andy Blackledge <me@andyblackledge.co.uk>
2024-07-24 14:16:20 +0100

    tweak lp setup

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,15 @@
 import { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
-    pageShots: {
-    pages: [ { path: '/', name: 'landing' } ],
-    baseUrl: 'http://172.17.0.1:3000',
-},
+    // pageShots: {
+    //     pages: [ { path: '/', name: 'landing' } ],
+    //     baseUrl: 'http://172.17.0.1:3000',
+    // },
+    storybookShots: {
+        storybookUrl: './storybook-static',
+    },
+    generateOnly: true,
+    failOnDifference: true,
     lostPixelProjectId: 'clyzugnzs31h9ws5pldz5ewu9',
     apiKey: process.env.LOST_PIXEL_API_KEY,
 };
